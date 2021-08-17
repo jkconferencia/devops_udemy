331aae45c0136e58e78bea3378168cc25fe3e8d3
jkconferencia <jkconferencia@gmail.com>
2021-08-16 20:10:30 -0500

    Prueba de integracion2222

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//PRUEBA CON LA INTEGRACION  A JENKINS
+//PRUEBA CON LA INTEGRACION  A JENKINS2222
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicApplicationTests {
