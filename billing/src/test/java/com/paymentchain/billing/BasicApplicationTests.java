8b6bea695906be3147c8f7a6966150f346b58c01
jkconferencia <jkconferencia@gmail.com>
2021-08-16 21:50:11 -0500

    Este es 00000

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-//PRUEBA CON LA INTEGRACION  A JENKINS2222444444777777
+//PRUEBA CON LA INTEGRACION  A JENKINS2222444444777777000000
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BasicApplicationTests {
